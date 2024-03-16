762f91cb12903a3472061f481f18c2d2e8b54cd7
RenzArcilla <renzarcilla09@gmail.com>
2024-03-16 23:10:50 +0800

    final

--- OOP/ASSIGNMET NO.2.py
+++ OOP/ASSIGNMET NO.2.py
@@ -1,1 +1,9 @@
-name = input("Please enter your name: ")
+from tkinter import *
+name = input("Please Enter your name: ")
+dream_job = input("What is your dream job? ")
+window = Tk()
+window.title("Future Career")
+window.geometry("500x300")
+label = Label(window, text=" " + "\n" + " " + "\n" + " " + "\n" + name + "\n" + "<< " + dream_job + " >>", font=("Arial", 20, "bold"))
+label.pack()
+window.mainloop()
